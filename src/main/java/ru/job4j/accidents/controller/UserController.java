c55dda719baa5b03ec86767190f983f8321fabbd
StanislavAerialist <moonlight_stas@mail.ru>
2023-06-12 13:51:16 +0500

    2. Регистрация пользователя [#296069]

--- src/main/java/ru/job4j/accidents/controller/UserController.java
+++ src/main/java/ru/job4j/accidents/controller/UserController.java
@@ -2,32 +2,37 @@ package ru.job4j.accidents.controller;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.JpaAuthorityRepository;
-import ru.job4j.accidents.repository.JpaUserRepository;
+import ru.job4j.accidents.service.SimpleUserService;
+import ru.job4j.accidents.service.UserService;
 
 @Controller
 public class UserController {
     private final PasswordEncoder encoder;
-    private final JpaUserRepository users;
+    private final UserService users;
     private final JpaAuthorityRepository authorities;
 
-    public UserController(PasswordEncoder encoder, JpaUserRepository users, JpaAuthorityRepository authorities) {
+    public UserController(PasswordEncoder encoder, SimpleUserService users, JpaAuthorityRepository authorities) {
         this.encoder = encoder;
         this.users = users;
         this.authorities = authorities;
     }
 
     @PostMapping("/reg")
-    public String regSave(@ModelAttribute User user) {
+    public String regSave(@ModelAttribute User user, Model model) {
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
-        users.save(user);
+        var savedUser = users.create(user);
+        if (savedUser.isEmpty()) {
+            model.addAttribute("error", "Пользователь с таким логином уже существует");
+            return "users/register";
+        }
         return "redirect:/login";
     }
 
--- src/main/java/ru/job4j/accidents/service/SimpleUserService.java
+++ src/main/java/ru/job4j/accidents/service/SimpleUserService.java
@@ -1,6 +1,8 @@
 package ru.job4j.accidents.service;
 
 import lombok.AllArgsConstructor;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.JpaUserRepository;
@@ -10,11 +12,18 @@ import java.util.Optional;
 @Service
 @AllArgsConstructor
 public class SimpleUserService implements UserService {
+    private static final Logger LOG = LogManager.getLogger(SimpleUserService.class.getName());
     private final JpaUserRepository userRepository;
 
     @Override
     public Optional<User> create(User user) {
-        userRepository.save(user);
-        return Optional.of(user);
+        Optional<User> rsl = Optional.empty();
+        try {
+            userRepository.save(user);
+            rsl = Optional.of(user);
+        } catch (Exception e) {
+            LOG.debug(e.getMessage(), e);
+        }
+        return rsl;
     }
 }
